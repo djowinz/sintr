fe23bd6fbf8e4d1e37a2a3fb6be9ac9dc6e34de0
Dyllen Owens <dyllen@owinz.io>
2026-02-14 15:51:36 -0600

    Adding in basic windowing and simple interaction logic between Rust and Slint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+slint::include_modules!();
+
+fn main() -> Result<(), slint::PlatformError> {
+    let ui = AppWindow::new()?;
+
+    ui.on_select_files_clicked({
+        let ui_handle = ui.as_weak();
+        move || {
+            let ui = ui_handle.unwrap();
+            println!("Button Clicked! Ready to open the file picker!");
+        }
+    });
+
+    ui.on_start_cleaning_clicked(|| {
+        println!("Stripping process clicked!");
+    });
+
+    ui.run()?;
+
+    Ok(())
 }
